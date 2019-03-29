573e058d50fd4b28f9abb4f2232b73cca13245b9
wknoben <45757529+wknoben@users.noreply.github.com>
2019-03-29 11:00:44 +0000

    Update optimoptions.m

--- Octave/optimoptions.m
+++ Octave/optimoptions.m
@@ -12,8 +12,15 @@ function [ opt ] = optimoptions( varargin )
 % This program is free software (GNU GPL v3) and distributed WITHOUT ANY
 % WARRANTY. See <https://www.gnu.org/licenses/> for details.
 
+% Set options
 opt = optimset('MaxFunEvals',1000);  
 
+% Display a warning that Octave is used. User should disable this if they are intentionally using Octave
+disp('---')
+disp('You are currently using MARRMoT''s Octave version.') 
+disp('If this is intentional, please navigate to ./MARRMoT/Octave/optimoptions.m and disable this warning on lines 19-23 of the file.')
+disp('If you want to use MARRMoT''s Matlab version, you must REMOVE the folder ./MARRMoT/Octave from your system. If you do not remove this folder, Matlab will not utilize its more powerful solver settings because it will preferentially navigate to the Octave folder and use those settings instead.')
+disp('---')
 
 end
 

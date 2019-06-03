f07ad9609b36667d992f85df3714c580f714b30c
wknoben <45757529+wknoben@users.noreply.github.com>
2019-06-03 11:52:48 +0200

    Added 'pause' statement

--- Octave/optimoptions.m
+++ Octave/optimoptions.m
@@ -21,6 +21,7 @@ disp('You are currently using MARRMoT''s Octave version.')
 disp('If this is intentional, please navigate to ./MARRMoT/Octave/optimoptions.m and disable this warning on lines 19-23 of the file.')
 disp('If you want to use MARRMoT''s Matlab version, you must REMOVE the folder ./MARRMoT/Octave from your system. If you do not remove this folder, Matlab will not utilize its more powerful solver settings because it will preferentially navigate to the Octave folder and use those settings instead.')
 disp('---')
+pause
 
 end
 

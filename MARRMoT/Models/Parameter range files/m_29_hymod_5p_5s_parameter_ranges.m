236fb8e0d3fac8c1007fb85d1ec5c76a6efb1aff
wknoben <45757529+wknoben@users.noreply.github.com>
2019-06-19 10:34:38 +0100

    fixed kf parameter description

--- MARRMoT/Models/Parameter range files/m_29_hymod_5p_5s_parameter_ranges.m
+++ MARRMoT/Models/Parameter range files/m_29_hymod_5p_5s_parameter_ranges.m
@@ -12,12 +12,12 @@ function [ theta ] = m_29_hymod_5p_5s_parameter_ranges( )
 %
 % Wagener, T., Boyle, D. P., Lees, M. J., Wheater, H. S., Gupta, Hoshin, 
 % V., & Sorooshian, S. (2001). A framework for development and application 
-% of hydrological models. Hydrology and Earth System Sciences, 5, 13–26.
+% of hydrological models. Hydrology and Earth System Sciences, 5, 13â€“26.
 
 theta = [1  , 2000;     % Smax, Maximum soil moisture storage [mm], 
          0  , 10;       % b, Soil depth distribution parameter [-]
          0  , 1;        % a, Runoff distribution fraction [-]
-         0  , 1;        % kf, base flow time parameter [d-1]
+         0  , 1;        % kf, fast flow time parameter [d-1]
          0  , 1];       % ks, base flow time parameter [d-1]
      
 % Display an overview and warning      

6cb428016cd4b578aaf0d7c830b5a3f4c21e8c83
wknoben <wouter.knoben@usask.ca>
2020-10-18 16:58:43 -0600

    Added Octave functionality for workflow example 4

---
+++ Octave/array2table.m
@@ -0,0 +1,9 @@
+% Octave function: transforms an array into a table.
+%
+% Copyright (C) 2020 M. K. Türkeri
+% This program is free software (GNU GPL v3) and distributed WITHOUT ANY
+% WARRANTY. See <https://www.gnu.org/licenses/> for details.
+
+function [ out ] = array2table(table)
+      out.Data=table;
+end

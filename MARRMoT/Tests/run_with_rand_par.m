0b47a9d024a77259a19d0509f3f2fe0886ce513e
Luca Trotter <l.trotter@unimelb.edu.au>
2021-11-03 10:25:41 +1100

    Edit function to run with random parameters
    just matching the current format, this function hadn't been updated in a
    while.

--- MARRMoT/Tests/run_with_rand_par.m
+++ MARRMoT/Tests/run_with_rand_par.m
@@ -7,32 +7,27 @@ load MARRMoT_example_data.mat
 input_climatology.precip   = data_MARRMoT_examples.precipitation;                   % Daily data: P rate  [mm/d]
 input_climatology.temp     = data_MARRMoT_examples.temperature;                     % Daily data: mean T  [degree C]
 input_climatology.pet      = data_MARRMoT_examples.potential_evapotranspiration;    % Daily data: Ep rate [mm/d]
-
-delta_t  = 1;                                                              % time step size of the inputs: 1 [d]
+input_climatology.delta_t  = 1;                                                     % time step size of the inputs: 1 [d]
 
 % Initiate model object and set random parameter set
-m = feval(model, delta_t);
+m = feval(model);
 m.theta = m.parRanges(:,1) + rand(m.numParams,1).*(m.parRanges(:,2)-m.parRanges(:,1));
 
 % Create vector of initial store values
 input_s0 = zeros(1, m.numStores);
  
 % Create struct for solver options
-solver_opts.resnorm_tolerance = 0.1;                                       % Root-finding convergence tolerance
-solver_opts.resnorm_maxiter   = 6;                                         % Maximum number of re-runs
-solver_opts.NewtonRaphson = optimset('MaxIter', m.numStores * 10);
-solver_opts.fsolve = optimoptions('fsolve',...
-                                  'Display','none',...                     % Disable display settings
-                                  'JacobPattern', m.JacobPattern);
-solver_opts.lsqnonlin = optimoptions('lsqnonlin',...                       % lsqnonlin settings for cases where fsolve fails
-                                     'Display','none',...
-                                     'JacobPattern',m.JacobPattern,...
-                                      'MaxFunEvals',1000);
-                                  
+input_solver_opts.resnorm_tolerance = 1e-10;                                       % Root-finding convergence tolerance
+input_solver_opts.rerun_maxiter   = 6;                                           % Maximum number of re-runs
+
+m.input_climate = input_climatology;
+m.solver_opts   = input_solver_opts;
+m.S0            = input_s0;
+
 % Run the model
 [output.ex,...
  output.in,...
- output.ss] = ...
-            m.get_output(input_climatology,...
-                         input_s0,...
-                         solver_opts);
+ output.ss,...
+ output.wb,...
+ output.solver_info] = ...
+            m.get_output();
